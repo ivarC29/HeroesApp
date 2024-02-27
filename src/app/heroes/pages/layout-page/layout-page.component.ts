692a520f85632f8b30a97460305e1b11b8062415
Noldia Chavez <ivar.n.c.29@gmail.com>
2024-02-27 15:54:36 -0500

    Angular Material

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', url: './list'},
+    { label: 'Añadir', icon: 'add', url: './new'},
+    { label: 'Buscar', icon: 'search', url: './search'},
+  ]
+
 }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  baseURL: 'https://dominio-web.dev/api'
+}
